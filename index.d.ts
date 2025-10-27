574db6f1353554734227aaf22d3f329bd18090bc
olimsaidov <olim.mail@gmail.com>
2025-10-27 11:19:54 +0000

    Deploying to gh-pages from @ HealthSamurai/aidbox-forms-renderer@65c67b8d186d1ba6b91b7503751486ae9a2ff699 🚀

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,8 @@
-import { Attachment } from 'fhir/r5';
-import { Coding } from 'fhir/r5';
 import { JSX } from 'react/jsx-runtime';
-import { Quantity } from 'fhir/r5';
 import { Questionnaire } from 'fhir/r5';
-import { QuestionnaireItem } from 'fhir/r5';
-import { QuestionnaireItemAnswerOption } from 'fhir/r5';
 import { QuestionnaireResponse } from 'fhir/r5';
-import { QuestionnaireResponseItem } from 'fhir/r5';
-import { QuestionnaireResponseItemAnswer } from 'fhir/r5';
-import { Reference } from 'fhir/r5';
 
-export declare type AnswerPrimitive = string | number | boolean | Coding | Attachment | Reference | Quantity;
-
-export { Attachment }
-
-export { Coding }
-
-export { Quantity }
-
-export { Questionnaire }
-
-export { QuestionnaireItem }
-
-export { QuestionnaireItemAnswerOption }
-
-export { QuestionnaireResponse }
-
-export { QuestionnaireResponseItem }
-
-export { QuestionnaireResponseItemAnswer }
-
-export { Reference }
-
-declare function Renderer({ questionnaire, initialResponse, onChange, onSubmit, }: RendererProps): JSX.Element;
+declare function Renderer({ questionnaire, initialResponse, onSubmit, onChange, }: RendererProps): JSX.Element;
 export default Renderer;
 
 declare type RendererProps = {
