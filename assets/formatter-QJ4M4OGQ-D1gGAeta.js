41fd20529fb2793824415c50a400e98c223de208
olimsaidov <olim.mail@gmail.com>
2025-12-12 12:09:38 +0000

    Deploying to gh-pages from @ HealthSamurai/aidbox-forms-renderer@43190430f1df1feabf01a4888adf66b18f354f5b 🚀

---
+++ assets/formatter-QJ4M4OGQ-D1gGAeta.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CSi0bO90.js";import{H as t}from"./iframe-CSUbJol3.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-DdyQG9CR.js";import"./index-DxUqIoLR.js";import"./index-k9bGYuq7.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
